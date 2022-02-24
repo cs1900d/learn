44a9ded75218cca157f73220e67a226c5b4844c8
cs1900d <100333317+cs1900d@users.noreply.github.com>
2022-02-24 20:22:27 +0800

    Add files via upload

---
+++ hello.java
@@ -0,0 +1,14 @@
+package hhhh;
+
+public class hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    // final boolean BFLAG=false;
+     //if(BFLAG)
+    	// System.out.println("Always Print Out Me!");
+     //else
+    	 System.out.println("hello world!");
+	}
+
+}
